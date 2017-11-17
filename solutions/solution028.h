fc71b769d78c0834d25da85755337243999bf480
wangyiguang <yiguang.wyg@antfin.com>
2017-11-18 00:15:39 +0800

    initialize framework.

---
+++ solutions/solution028.h
@@ -0,0 +1,21 @@
+//
+// Created by wangyiguang on 17/11/17.
+//
+
+#ifndef ARENA_CPP_SOLUTION28_H
+#define ARENA_CPP_SOLUTION28_H
+
+#include <string>
+
+using namespace std;
+
+class Solution028 {
+public:
+    int strStr(string haystack, string needle);
+    static void main(int argc, char **argv) {
+        Solution028 solution;
+        solution.strStr(NULL, NULL);
+    }
+};
+
+#endif //ARENA_CPP_SOLUTION28_H
---
+++ solutions/sulution028.cpp
@@ -0,0 +1,14 @@
+//
+// Created by wangyiguang on 17/11/17.
+//
+
+#include "solution028.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int Solution028::strStr(string haystack, string needle) {
+    cout << "hello world";
+}
