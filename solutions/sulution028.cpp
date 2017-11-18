3d9a0d049fede5375935f82bb807f6102cbe1590
wangyiguang <yiguang.wyg@antfin.com>
2017-11-18 19:48:12 +0800

    add solution028.

--- solutions/sulution028.cpp
+++ solutions/sulution028.cpp
@@ -10,10 +10,29 @@ using namespace std;
 
 
 int Solution028::strStr(string haystack, string needle) {
-    cout << "hello world" << endl;
-    return 0;
+    if (haystack.size() == 0 || needle.size() == 0) {
+        return 0;
+    }
+
+    int cur;
+    bool match;
+    for (int i = 0; i < haystack.size(); i++) {
+        match = true;
+        cur = i;
+        for (int j = 0; j < needle.size(); j++) {
+            if (haystack[cur] != needle[j]) {
+                match = false;
+                break;
+            }
+            cur += 1;
+        }
+        if (match) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 void Solution028::run() {
-    strStr("", "");
+    cout << strStr("hello world", "ll") << endl;
 }
